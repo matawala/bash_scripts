4d572f623852f6b50d66d63752d78bd64137b0e4
Bong <Bong@matawala.com>
2024-04-22 20:57:50 -0400

    Edit div.sh and added mult.sh

--- calculator/mult.sh
+++ calculator/mult.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 read -p "Enter First Number : " fn
 read -p "Enter Second Number : " sn
-quotient=$(( $fn / $sn ))
-echo "The quotient of $fn and $sn is $quotient"
+product=$(( $fn * $sn ))
+echo "The product of $fn and $sn is $product"
